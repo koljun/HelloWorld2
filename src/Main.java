4a11f1ce09540f5994c3573eb100bf2674d1809f
koljun <koljun@rambler.ru>
2017-01-16 14:34:36 -0500

    another line

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Here ye Here ye : important announcement");
         System.out.println("Hello World!");
+        System.out.println("Goodbye, user");
     }
 }
