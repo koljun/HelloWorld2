08be2118206814d1d5267ef27a50189a06768ffd
koljun <koljun@rambler.ru>
2017-01-16 14:33:17 -0500

    added one line

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Here ye Here ye : important announcement");
         System.out.println("Hello World!");
     }
 }
